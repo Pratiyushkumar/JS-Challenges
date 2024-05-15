c04d2c33cc14d77bbe46d4cd3b867309f34e970c
Pratiyush Kumar <pratiyush.inv@gmail.com>
2024-05-15 21:45:51 +0530

    star rating DOM challenge completed

--- starRating/script.js
+++ starRating/script.js
@@ -1,74 +1,78 @@
-const app = document.querySelector('#app');
-const emojisContainer = document.querySelector('#emojis');
+const totalStars = 5;
 
-function createStar(n) {
-  const result = [];
+let filled = 0;
+let unfilled = 0;
+let ratings = 0;
 
-  for (let i = 0; i < n; i++) {
-    const button = document.createElement('button');
-    button.classList.add('star-empty', 'star');
-    button.setAttribute(`data-index`, i);
-    result.push(button);
+const starContainer = document.querySelector('#app');
+
+starContainer.appendChild(
+  createElements(totalStars, (i) =>
+    createElement('button', { class: 'star star-empty', 'data-index': i })
+  )
+);
+
+const stars = document.querySelectorAll('.star');
+
+starContainer.addEventListener('mouseover', hoverMouse);
+starContainer.addEventListener('mouseleave', leaveMouse);
+starContainer.addEventListener('click', mouseClick);
+
+function createElement(element, attributes) {
+  const el = document.createElement(element);
+
+  for (let key in attributes) {
+    el.setAttribute(key, attributes[key]);
+  }
+  return el;
+}
+
+function createElements(count, fn, start = 0) {
+  const fragments = document.createDocumentFragment();
+
+  for (let i = 0; i < count; i++) {
+    fragments.appendChild(fn(i));
   }
 
-  return result;
+  return fragments;
 }
 
-function paintTheStar(star, allStars) {
-  const indexOfCurrentStar = star.dataset.index;
-  for (let i = indexOfCurrentStar; i >= 0; i--) {
-    allStars[i].classList.remove('star-empty');
-    allStars[i].classList.add('colorStar');
-  }
-  console.log(allStars);
-}
-
-function unPaintTheStar(star, allStars) {
-  const indexOfCurrentStar = star.dataset.index;
-  for (let i = indexOfCurrentStar; i >= 0; i--) {
-    allStars[i].classList.remove('colorStar');
-    allStars[i].classList.add('star-empty');
-  }
-}
-
-function giveTheRating(star, allStars) {
-  // unPaintTheStar(star, allStars);
-
-  console.log('clicked');
-  paintTheStar(star, allStars);
-}
-
-function hoverListener(star, allStars) {
-  paintTheStar(star, allStars);
-}
-
-function leaveListener(star, allStars) {
-  unPaintTheStar(star, allStars);
-}
-
-function main() {
-  const stars = createStar(5);
-
-  for (let i = 0; i < stars.length; i++) {
-    app.appendChild(stars[i]);
+function fillStars(count) {
+  console.log('filled value', filled);
+  for (let i = filled; i <= count; i++) {
+    console.log('values of i', i);
+    stars[i].classList.remove('star-empty');
+    stars[i].classList.add('colorStar');
   }
 
-  const getAllStars = document.querySelectorAll('.star');
+  for (let i = count; i <= unfilled; i++) {
+    console.log('i of unfilled', i);
+    stars[i].classList.remove('colorStar');
+    stars[i].classList.add('star-empty');
+  }
 
-  getAllStars.forEach((star) => {
-    star.addEventListener('mouseover', () => {
-      hoverListener(star, getAllStars);
-    });
-    star.addEventListener('mouseleave', () => {
-      leaveListener(star, getAllStars);
-    });
-
-    // star.addEventListener('click', () => {
-    //   giveTheRating(star, getAllStars);
-    // });
-    star.removeEventListener('mouseleave', leaveListener);
-    star.removeEventListener('mouseover', hoverListener);
-  });
+  filled = count;
+  unfilled = count;
 }
 
-main();
+function mouseClick(e) {
+  const target = e.target;
+  if (target.classList.contains('star')) {
+    ratings = +target.dataset.index;
+    console.log('ratings', ratings);
+    fillStars(ratings);
+  }
+}
+
+function hoverMouse(e) {
+  const target = e.target;
+  if (target.classList.contains('star')) {
+    const index = target.dataset.index;
+    console.log('index from mouseHover', index);
+    fillStars(+index);
+  }
+}
+
+function leaveMouse() {
+  fillStars(+ratings);
+}
